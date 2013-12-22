eb67f78beb86f8c9ce067911d2ebe4ae5623774e
Pathana Chonmany <admin@pchonmany.com>
2013-12-22 18:15:54 -0500

    Updated About page

--- views/v_about_index.php
+++ views/v_about_index.php
@@ -2,6 +2,15 @@
 	<h1>About</h1>
 		Band Central was established in 2013. The mission of Band Central is to connect the fans (users) with their favorite bands. They are able to do so by following the band on this site to get all the latest posts and updates from the band. We continue to gradually grow everyday, adding more and more of your favorite most known bands in the world, for you to be able to have easier and faster connections to their latest news updates posted from the bands themselves!
 	<p><br>
-		
+	<hr>
+	<p><br>
+		• Register/login to the site<br> 
+		• View and edit user profile<br>
+		• View a list of all bands on site<br> 
+		• Follow bands to get their latest new updates from the band themselves<br>
+		• Unfollow bands<br>
+		• Dashboard to view all posts from the bands you follow<br><br>
+
+		All forms in the site use JavaScript validation.
 
 </div>
