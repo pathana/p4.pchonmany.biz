c1486422d7b5d6bc98bdcebb7642028ca0c5f4cd
Pathana Chonmany <admin@pchonmany.com>
2013-12-22 18:38:50 -0500

    Updated 'welcome' message on homepage

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,6 +1,9 @@
 <div id="content">
 	<h1>Welcome<?php if($user) echo ' '.$user->first_name; + ' !'?></h1>
-			Welcome to BlogByte, the one and only microblog that is rapidly growing in user preference of the best microblog to use on the web today. A quick and easy, less than 30 second sign up and you'll be on your way. Our microblog includes adding new posts, following and/or unfollowing other users in BlogByte, seeing all of the posts of the user(s) you're following, 
+			Welcome to Band Central, the only network to stay up-to-date with your favorite bands, getting instant updates from the bands themselves! An easy registration
+			 that takes less than thirty seconds to do, will give the ability to follow your favorite bands. You can view and edit your profile, and easy login of signing in 
+			 with your username and email. We recently have just added two time Grammy winners, Phoenix! They are about to head out to tour soon. Get tickets to get to see them 
+			 live while tickets last! Enjoy your stay here! Look out for more site updates on adding more bands everyday!
 
 		<div id="footer">
 			Band Central Created by Pathana Chonmany for P4 DWA15
